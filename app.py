4f20e9a3be47b2474d990c57b69fea9daf32ee2f
Patrick <wpmjones@gmail.com>
2021-02-21 07:52:24 -0800

    attempting flask

--- app.py
+++ app.py
@@ -1,52 +1,89 @@
-import creds
-import os
+import logging
 
-# from loguru import logger
-from slack_bolt.async_app import AsyncApp
+logging.basicConfig(level=logging.DEBUG)
 
-app = AsyncApp(token=creds.bot_token,
-               signing_secret=creds.signing_secret)
+from slack_bolt import App
+from slack_bolt.adapter.flask import SlackRequestHandler
+
+app = App()
 
 
-# Listens to incoming messages that contain "hello"
-@app.message("hello")
-async def message_hello(message, say):
-    # say() sends a message to the channel where the event was triggered
-    say(
-        blocks=[
-            {
-                "type": "section",
-                "text": {"type": "mrkdwn", "text": f"Hey there <@{message['user']}>!"},
-                "accessory": {
-                    "type": "button",
-                    "text": {"type": "plain_text", "text": "Click Me"},
-                    "action_id": "button_click",
-                },
-            }
-        ],
-        text=f"Hey there <@{message['user']}>!",
-    )
+@app.middleware  # or app.use(log_request)
+def log_request(logger, body, next):
+    logger.debug(body)
+    return next()
 
 
 @app.event("app_mention")
-async def event_test(body, say, logger):
+def event_test(body, say, logger):
     logger.info(body)
     say("What's up?")
 
 
-@app.command("/hello-bolt-python")
-async def hello(body, ack):
-    user_id = body["user_id"]
-    await ack(f"Hi <@{user_id}>!")
+@app.event("message")
+def handle_message():
+    pass
 
 
-@app.action("button_click")
-async def action_button_click(body, ack, say):
-    # Acknowledge the action
-    ack()
-    say(f"<@{body['user']['id']}> clicked the button")
+from flask import Flask, request
+
+flask_app = Flask(__name__)
+handler = SlackRequestHandler(app)
 
 
-# Start your app
-if __name__ == "__main__":
-    app.start(port=3000)  # int(os.environ.get("PORT", 3000)))
+@flask_app.route("/slack/events", methods=["POST"])
+def slack_events():
+    return handler.handle(request)
+
+# import creds
+# import os
+#
+# # from loguru import logger
+# from slack_bolt.async_app import AsyncApp
+#
+# app = AsyncApp(token=creds.bot_token,
+#                signing_secret=creds.signing_secret)
+#
+#
+# # Listens to incoming messages that contain "hello"
+# @app.message("hello")
+# async def message_hello(message, say):
+#     # say() sends a message to the channel where the event was triggered
+#     say(
+#         blocks=[
+#             {
+#                 "type": "section",
+#                 "text": {"type": "mrkdwn", "text": f"Hey there <@{message['user']}>!"},
+#                 "accessory": {
+#                     "type": "button",
+#                     "text": {"type": "plain_text", "text": "Click Me"},
+#                     "action_id": "button_click",
+#                 },
+#             }
+#         ],
+#         text=f"Hey there <@{message['user']}>!",
+#     )
+#
+#
+# @app.event("app_mention")
+# async def event_test(body, say, logger):
+#     logger.info(body)
+#     say("What's up?")
+#
+#
+# @app.command("/hello-bolt-python")
+# async def hello(body, ack):
+#     user_id = body["user_id"]
+#     await ack(f"Hi <@{user_id}>!")
+#
+#
+# @app.action("button_click")
+# async def action_button_click(body, ack, say):
+#     # Acknowledge the action
+#     ack()
+#     say(f"<@{body['user']['id']}> clicked the button")
+#
+#
+# # Start your app
+# if __name__ == "__main__":
+#     app.start(port=3000)  # int(os.environ.get("PORT", 3000)))
