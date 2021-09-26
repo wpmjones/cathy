e8faa8f0aa786bc447ffbcaacca270960cbd25aa
W. Patrick Jones <wpmjones@gmail.com>
2021-09-26 13:11:20 -0700

    add scraper for OOS notifications

--- scraper.py
+++ scraper.py
@@ -68,9 +68,12 @@ def check_cem():
         for key, value in score_dict.items():
             content += f"{key}{' '*(25-len(key))}{' '*(4-len(value))}{value}\n"
         content += "```"
-        logger.info(content)
         payload = {"text": content}
         r = requests.post(webhook_url, json=payload)
+        if r.status_code != 200:
+            raise ValueError(f"Request to Slack returned an error {r.status_code}\n"
+                             f"The response is: {r.text}")
+
 
 def check_oos():
     search_date = datetime.date.today() - datetime.timedelta(days=1)
@@ -82,20 +85,51 @@ def check_oos():
     for i in id_list:
         typ, data = mail.fetch(i, "(RFC822)")
         raw = data[0][1]
-        raw_str = raw.decode("utf-9")
-        msg = email.message_from_string(raw_str)
+        msg = email.message_from_bytes(raw)
         if msg.is_multipart():
             for part in msg.walk():
                 part_type = part.get_content_type()
                 if part_type == "text/plain" and "attachment" not in part:
-                    body = part.get_payload(decode=True).decode("utf-8")
+                    body = part.get_payload()
                 if part.get("Content-Disposition") is None:
                     pass
         else:
-            body = msg.get_payload(decode=True).decode("utf-8")
-    # body captured, search for relevant text
-
+            body = msg.get_payload()
+        # body captured, search for relevant text
+        if body:
+            start = body.find("item #") + 1
+            end = start + 14
+            item_number = "I" + body[start:end].strip()
+            start = end + 2
+            end = body.find("This item was") - 1
+            item_name = body[start:end].strip()
+            item = f"{item_number} - {item_name}"
+            start = end
+            end = body.find("The product is")
+            truck_date = body[start:end].strip() + "."
+            start = end
+            end = body.find("pending") + 12
+            rescheduled = body[start:end].strip() + " supplier's availability."
+            # post content to Slack
+            content = f"*{item}*\n{truck_date}\n{rescheduled}"
+            payload = {
+                "text": "Out of Stock Notification",
+                "blocks": [
+                    {
+                        "type": "section",
+                        "text": {
+                            "type": "mrkdwn",
+                            "text": content
+                        }
+                    }
+                ]
+            }
+            r = requests.post(creds.webhook_boh, json=payload)
+            if r.status_code != 200:
+                raise ValueError(f"Request to Slack returned an error {r.status_code}\n"
+                                 f"The response is: {r.text}")
 
 
 if __name__ == "__main__":
     check_cem()
+    check_oos()
