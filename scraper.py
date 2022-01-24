4e0bf60b277a11468084f3f898d2529af5b0c316
Patrick <wpmjones@gmail.com>
2022-01-23 18:21:27 -0800

    more Symbol functionality

--- scraper.py
+++ scraper.py
@@ -1,8 +1,10 @@
 import creds
 import datetime
 import email
+import gspread
 import imaplib
 import requests
+import time
 
 mail = imaplib.IMAP4_SSL("imap.gmail.com")
 mail.login(creds.gmail_u, creds.gmail_app)
@@ -199,7 +201,27 @@ def check_oos():
                                  f"The response is: {r.text}")
 
 
+def post_symbol_goal():
+    # Connect to Google Sheets
+    gc = gspread.service_account(filename=creds.gspread)
+    sh = gc.open_by_key(creds.symbol_id)
+    sheet = sh.worksheet("Daily Goals")
+    current_date = datetime.date.today()
+    cell = sheet.find(current_date.strftime("%Y-%m-%d"))
+    goal = sheet.cell(cell.row, cell.col+6).value
+    content = f"**Today's Symbol Goal:** {goal}"
+    print(content)
+    # payload = {"text": content}
+    # r = requests.post(creds.webhook_all, json=payload)
+    # if r.status_code != 200:
+    #     raise ValueError(f"Request to Slack returned an error {r.status_code}\n"
+    #                      f"The response is: {r.text}")
+
+
 if __name__ == "__main__":
-    check_cem()
-    check_oos()
-    check_allocation()
+    # check_cem()
+    # check_oos()
+    # check_allocation()
+    if datetime.date.weekday(today) != 0:
+        time.sleep(60*60*3)  # sleep 3 hours
+    post_symbol_goal()
