57de6d20d1609e8b456ca6d75e728f9a008849a8
Patrick <wpmjones@gmail.com>
2022-06-15 13:41:10 -0700

    enhance catering reminders

--- cater_remind.py
+++ cater_remind.py
@@ -14,57 +14,96 @@ maps_url_base = "https://www.google.com/maps/search/?api=1&query="
 
 def main():
     """Notification of catering orders for each day"""
-    webhook_url = creds.webhook_cater
+    webhook_url = creds.webhook_test
 
     now = datetime.today().strftime("%m/%d/%Y")
     list_of_cells = sheet.findall(now, in_column=1)
-    list_of_rows = [x.row for x in list_of_cells]
-    blocks = []
-    for row in list_of_rows:
-        values_list = sheet.row_values(row)
-        blocks.append(
-            {
-                "type": "section",
-                "fields": [
+    if list_of_cells:
+        list_of_rows = [x.row for x in list_of_cells]
+        blocks = []
+        for row in list_of_rows:
+            values_list = sheet.row_values(row)
+            if values_list[2] == "PICKUP":
+                blocks.append(
                     {
-                        "type": "mrkdwn",
-                        "text": f"*Time:*\n{values_list[1]}"
-                    },
-                    {
-                        "type": "mrkdwn",
-                        "text": f"*Driver:*\n{values_list[2]}"
-                    },
-                    {
-                        "type": "mrkdwn",
-                        "text": f"*Customer:*\n{values_list[3]}"
-                    },
-                    {
-                        "type": "mrkdwn",
-                        "text": f"*Phone:*\n{values_list[5]}"
-                    },
-                    {
-                        "type": "mrkdwn",
-                        "text": f"*Address:*\n<{maps_url_base + values_list[4].replace(' ', '%20')}|{values_list[4]}>"
+                        "type": "header",
+                        "text": "Pickup"
                     }
-                ]
-            }
-        )
-        blocks.append(
-            {
-                "type": "divider"
-            }
-        )
-    blocks = blocks[:-1]
+                )
+                blocks.append(
+                    {
+                        "type": "section",
+                        "fields": [
+                            {
+                                "type": "mrkdwn",
+                                "text": f"*Time:*\n{values_list[1]}"
+                            },
+                            {
+                                "type": "mrkdwn",
+                                "text": f"*Customer:*\n{values_list[3]}"
+                            },
+                            {
+                                "type": "mrkdwn",
+                                "text": f"*Phone:*\n{values_list[5]}"
+                            }
+                        ]
+                    }
+                )
+                blocks.append(
+                    {
+                        "type": "divider"
+                    }
+                )
+            else:
+                blocks.append(
+                    {
+                        "type": "header",
+                        "text": "Delivery"
+                    }
+                )
+                blocks.append(
+                    {
+                        "type": "section",
+                        "fields": [
+                            {
+                                "type": "mrkdwn",
+                                "text": f"*Time:*\n{values_list[1]}"
+                            },
+                            {
+                                "type": "mrkdwn",
+                                "text": f"*Driver:*\n{values_list[2]}"
+                            },
+                            {
+                                "type": "mrkdwn",
+                                "text": f"*Customer:*\n{values_list[3]}"
+                            },
+                            {
+                                "type": "mrkdwn",
+                                "text": f"*Phone:*\n{values_list[5]}"
+                            },
+                            {
+                                "type": "mrkdwn",
+                                "text": f"*Address:*\n<{maps_url_base + values_list[4].replace(' ', '%20')}|{values_list[4]}>"
+                            }
+                        ]
+                    }
+                )
+                blocks.append(
+                    {
+                        "type": "divider"
+                    }
+                )
+        blocks = blocks[:-1]
 
-    payload = {
-        "text": "Catering Deliveries for Today",
-        "blocks": blocks
-    }
+        payload = {
+            "text": "Catering Orders for Today",
+            "blocks": blocks
+        }
 
-    r = requests.post(webhook_url, json=payload)
-    if r.status_code != 200:
-        raise ValueError(f"Request to Slack returned an error {r.status_code}\n"
-                         f"The response is: {r.text}")
+        r = requests.post(webhook_url, json=payload)
+        if r.status_code != 200:
+            raise ValueError(f"Request to Slack returned an error {r.status_code}\n"
+                             f"The response is: {r.text}")
 
 
 if __name__ == "__main__":
