f191539035e9d9e3ff6861c52ea66ca9e09d7c41
W. Patrick Jones <wpmjones@users.noreply.github.com>
2022-07-27 10:18:26 -0700

    Create weather_check.py

---
+++ weather_check.py
@@ -0,0 +1,23 @@
+import creds
+import requests
+
+from loguru import logger
+
+logger.add("weather.log", rotation="1 week")
+
+lat = "36.06302829757474"
+lon = "-115.17006319892765"
+weather_url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={creds.weather_key}"
+
+
+def main():
+    """Notify team when temps are over 100 degrees"""
+    response = requests.get(weather_url)
+    data = response.json()
+    current_temp = data['main']['temp']
+    feels_like = data['main']['feels_like']
+    logger.info(f"{current_temp} and {feels_like}")
+  
+  
+if __name__ == "__main__":
+    main()
