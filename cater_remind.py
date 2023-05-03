43e9254e0e431f5b7aad001bae7cadceb75fd169
Patrick <wpmjones@gmail.com>
2023-05-02 20:10:20 -0700

    additional cater reminder

--- cater_remind.py
+++ cater_remind.py
@@ -12,15 +12,13 @@ sheet1 = spreadsheet.worksheet("Sheet1")
 
 now = datetime.today().strftime("%m/%d/%Y")
 maps_url_base = "https://www.google.com/maps/search/?api=1&query="
-webhook_url = creds.webhook_cater
+webhook_url = creds.test_channel  # creds.webhook_cater
 
 
 def morning():
     """Notification of catering orders for each day (details)"""
     list_of_orders = sheet1.findall(now, in_column=1)
     list_of_rows = [x.row for x in list_of_orders]
-    print(list_of_orders)
-    print(list_of_rows)
     blocks = []
     for row in list_of_rows:
         values_list = sheet1.row_values(row)
@@ -110,16 +108,48 @@ def morning():
         "blocks": blocks
     }
 
-    # r = requests.post(webhook_url, json=payload)
-    # if r.status_code != 200:
-    #     raise ValueError(f"Request to Slack returned an error {r.status_code}\n"
-    #                      f"The response is: {r.text}")
+    r = requests.post(webhook_url, json=payload)
+    if r.status_code != 200:
+        raise ValueError(f"Request to Slack returned an error {r.status_code}\n"
+                         f"The response is: {r.text}")
 
 
 def evening():
     """Notification of catering orders for upcoming days (summary)"""
-    list_of_orders = sheet1.get_values()
-    blocks = []
+    list_of_orders = sheet1.get_values("A368:F380")
+    list_of_rows = [x.row for x in list_of_orders]
+    list_of_deliveries = []
+    blocks = [
+        {
+            "type": "header",
+            "text": {"type": "plain_text", "text": "Delivery"}
+        }
+    ]
+    for row in list_of_rows:
+        print(row)
+        values_list = sheet1.row_values(row)
+        print(values_list)
+        if values_list[2] != "PICKUP":
+            sheet2 = spreadsheet.worksheet("Sheet2")
+            driver_list = sheet2.get_all_values()
+            driver_name = values_list[2].strip()
+            for driver in driver_list:
+                if driver[0] == driver_name:
+                    driver_tag = f"<@{driver[1]}>"
+                    break
+            else:
+                driver_tag = driver_name
+            list_of_deliveries.append(f"{values_list[0]} - {values_list[1]} - {driver_tag}")
+    new_line = "\n"
+    blocks.append(
+        {
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": new_line.join(list_of_deliveries)
+            }
+        }
+    )
 
 
     payload = {
@@ -127,10 +157,10 @@ def evening():
         "blocks": blocks
     }
 
-    # r = requests.post(webhook_url, json=payload)
-    # if r.status_code != 200:
-    #     raise ValueError(f"Request to Slack returned an error {r.status_code}\n"
-    #                      f"The response is: {r.text}")
+    r = requests.post(webhook_url, json=payload)
+    if r.status_code != 200:
+        raise ValueError(f"Request to Slack returned an error {r.status_code}\n"
+                         f"The response is: {r.text}")
 
 
 if __name__ == "__main__":
