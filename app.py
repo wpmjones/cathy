15d44d92beaaca4e81a81bd7cffd2adc7c6b921d
Patrick <wpmjones@gmail.com>
2023-10-14 16:15:37 -0700

    add swap button - testing

--- app.py
+++ app.py
@@ -124,8 +124,9 @@ async def pull_notes(user_loc):
     all_elements = []
     temp_blocks = []
     for row in all_values:
+        logger.info(f"Row: {row}")
         if row[0] == leader_text:
-            value = row[2]
+            value = row[1]
             leader_elements.append(
                 {
                     "type": "rich_text_section",
@@ -133,7 +134,7 @@ async def pull_notes(user_loc):
                 }
             )
         if row[0] == all_text:
-            value = row[2]
+            value = row[1]
             all_elements.append(
                 {
                     "type": "rich_text_section",
