841e40db56af707da941e463ffbcc6fed604e32a
Patrick <wpmjones@gmail.com>
2023-10-21 20:00:16 -0700

    add waste report - testing

---
+++ waste_report.py
@@ -0,0 +1,51 @@
+import creds
+import gspread
+import requests
+
+from datetime import datetime
+
+# Connect to Google Sheets
+gc = gspread.service_account(filename=creds.gspread)
+spreadsheet = gc.open_by_key(creds.waste_id)
+
+
+def main():
+    webhook_url = creds.webhook_test
+    now_today = datetime.today()
+
+    sheet = spreadsheet.worksheet("Data")
+    num_rows = sheet.row_count
+    values = sheet.get(f"A{num_rows - 10}:J{num_rows}")
+    print(values)
+
+    blocks = [
+        {
+            "type": "section",
+            "block_id": "section_header",
+            "text": {
+                "type": "mrkdwn",
+                "text": (f"*Waste Report for {now_today}")
+            }
+        },
+        {
+            "type": "divider"
+        },
+        {
+            "type": "section",
+            "block_id": "waste_report"
+        }
+    ]
+
+    payload = {
+        "text": "Daily Waste Report",
+        "blocks": blocks
+    }
+
+    # r = requests.post(webhook_url, json=payload)
+    # if r.status_code != 200:
+    #     raise ValueError(f"Request to Slack returned an error {r.status_code}\n"
+    #                      f"The response is: {r.text}")
+
+
+if __name__ == "__main__":
+    main()
