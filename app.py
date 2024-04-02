a3fe8270dcc9f56b6969995b0c65647159b82bd7
Patrick <wpmjones@gmail.com>
2024-04-01 18:45:42 -0700

    add /cater [add or remove]

--- app.py
+++ app.py
@@ -687,6 +687,7 @@ async def cater(ack, command, body, client):
 
 @app.block_action("input_type")
 async def update_modal(ack, body, view, client):
+    logger.info(view['state'])
     cater_date = view['state']['values']['block_date']['input_date']['selected_date']
     cater_time = view['state']['values']['block_time']['input_time']['selected_time']
     cater_type = view['state']['values']['block_type']['input_type']['selected_option']
