4bb6e167bee7b97d12df3f599d404f18cdf1af97
Patrick <wpmjones@gmail.com>
2024-04-05 22:12:17 -0700

    fix wind speed

--- weather_check.py
+++ weather_check.py
@@ -15,7 +15,6 @@ def main():
     webhook_url = creds.webhook_foh
     response = requests.get(weather_url)
     data = response.json()
-    logger.info(data)
     current_temp = data['main']['temp']
     feels_like = data['main']['feels_like']
     wind_speed = data['wind']['speed']
