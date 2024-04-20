08035bda9f6098f4791bfedfe7109db329e985c8
Patrick <wpmjones@gmail.com>
2024-04-20 09:37:42 -0700

    log deleted messages

--- app.py
+++ app.py
@@ -3095,7 +3095,6 @@ async def find_names(ack, body, client):
         if ratio > fuzzy_number:
             count += 1
             tardy_text += f"\nTardy on {row[1]}"
-            logger.info(f"Tardy - {row[0]} matches {input_name}")
     if count == 0:
         tardy_text = f"No tardies found for {input_name}"
     # Collect Discipline
@@ -3108,7 +3107,6 @@ async def find_names(ack, body, client):
         if ratio > fuzzy_number:
             count += 1
             disc_text += f"\n{row[0]} - ({row[2]}) {row[3]}"
-        logger.info(f"Discpline - {row[1]} matches {input_name}")
     if count == 0:
         disc_text = f"No discpline found for {input_name}."
     blocks = [
